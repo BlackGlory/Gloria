022191a923f4caf9cfbf1ec1367ba1452bf505b1
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2016-07-22 00:09:36 +0800

    add create task modal

--- src/components/GloriaApp.vue
+++ src/components/GloriaApp.vue
@@ -5,29 +5,48 @@
       text-color-active="white" indicator-color="white"
     >
       <ui-tab icon="format_list_bulleted" header="Task">
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-task></gloria-task>
-        <gloria-fab @click="showNewDialogCode = true" color="primary" icon="add"></gloria-fab>
-        <ui-modal :show.sync="showNewDialogCode" header="New Task">
-          <textarea></textarea>
+        <div id="task-list">
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+          <gloria-task></gloria-task>
+        </div>
+        <gloria-fab type="mini" @click="showNewDialogCode = true" color="primary" icon="add"></gloria-fab>
+        <ui-modal :show.sync="showNewDialogCode" header="Paste your code">
+          <ui-textbox
+            label="Task Code"
+            :multi-line="true"
+            icon="code"
+            name="code"
+            :value.sync="code"
+            placeholder="Paste your fantastic code here"
+          ></ui-textbox>
           <div slot="footer">
             <ui-button @click="switchDialog('showNewDialogCode', 'showNewDialogName')" color="primary">Next</ui-button>
             <ui-button @click="showNewDialogCode = false">Cancel</ui-button>
           </div>
         </ui-modal>
-        <ui-modal :show.sync="showNewDialogName" header="New Task">
-          <input type="text" />
+        <ui-modal :show.sync="showNewDialogName" header="Give it a good name">
+          <ui-textbox name="name" :value.sync="name" label="Task Name" type="text" placeholder="Input a task name"></ui-textbox>
           <div slot="footer">
-            <ui-button @click="(showNewDialogName = false, createTask())" color="primary">Finish</ui-button>
+            <ui-button @click="switchDialog('showNewDialogName', 'showNewDialogConfig')" color="primary">Next</ui-button>
             <ui-button @click="switchDialog('showNewDialogName', 'showNewDialogCode')">Back</ui-button>
+          </div>
+        </ui-modal>
+        <ui-modal :show.sync="showNewDialogConfig" header="Finally some configuration">
+          <ui-slider :value.sync="triggerInterval" label="Trigger interval(minutes)" icon="event"></ui-slider>
+          <p>This task will trigger once every {{ triggerInterval }} min(s).</p>
+          <ui-checkbox v-el:is-notice-again :value.sync="isNoticeAgain">Notice until an interaction</ui-checkbox>
+          <ui-tooltip :trigger="$els.isNoticeAgain" position="bottom left" content="It means that if I ignored it, notice me again."></ui-tooltip>
+          <div slot="footer">
+            <ui-button @click="(showNewDialogConfig = false, createTask())" color="primary">Finish</ui-button>
+            <ui-button @click="switchDialog('showNewDialogConfig', 'showNewDialogName')">Back</ui-button>
           </div>
         </ui-modal>
       </ui-tab>
@@ -60,18 +79,34 @@ export
   data: ->
     show-new-dialog-code: false
     show-new-dialog-name: false
+    show-new-dialog-config: false
+    code: ''
+    name: ''
+    trigger-interval: 5
+    is-notice-again: false
   methods:
     switch-dialog: (current, next) ->
       this.$data[current] = false
       this.$data[next] = true
     create-task: ->
-      console.log this
+      this.$data.code = ''
+      this.$data.name = ''
+      this.$data.trigger-interval = 5
+      this.$data.is-notice-again = false
 </script>
 
 <style lang="stylus">
+#task-list .gloria-task:not(:last-child)
+  margin-bottom: 8px
+
 .ui-tabs-body
-  height: calc(100vh - 72px)
+  padding-top: 8px
+  padding-bottom: 8px
+  height: calc(100vh - 72px - 16px)
   overflow-y: auto
+
+.ui-textbox-textarea
+  min-height: 200px
 
 .gloria-fab
   z-index: 1
