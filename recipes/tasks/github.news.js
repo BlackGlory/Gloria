b8323728c9996fdd6ca2cdf4fe84ea4ea4e7ce6c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2016-09-11 11:58:15 +0800

    update recipe

--- recipes/tasks/github.news.js
+++ recipes/tasks/github.news.js
@@ -2,13 +2,33 @@ Promise.all([
   importScripts('gloria-utils')
 , fetch('https://github.com/').then(res => res.text())
 ])
-.then(([{ cheerio }, html]) => {
+.then(([{ cheerio, lodash }, html]) => {
   let $ = cheerio.load(html)
-  return $('.simple .title').map((i, el) => {
-    return {
-      title: $(el).text().trim()
-    , url: 'https://github.com' + $(el).find('a:last-child').attr('href')
-   }
- }).get()
+    , users = lodash.union(
+    $('.title a:first-of-type')
+    .map((i, el) => $(el).attr('href').split('/').filter(x => !!x))
+    .get()
+  )
+  return fetch('https://api.github.com/search/users?q=' + encodeURIComponent(
+    users
+    .map(x => `user:${x}`)
+    .join(' ')
+  ))
+  .then(res => res.json())
+  .then(({ items }) => {
+    let dict = {}
+    items.forEach(item => dict[item.login] = item.avatar_url)
+    return dict
+  })
+  .then(dict => {
+    return $('.simple .title, .alert .title').map((i, el) => {
+      let userName = $(el).find('a:first-of-type').attr('href').split('/')[1]
+      return {
+        title: $(el).text().trim()
+      , url: 'https://github.com' + $(el).find('a:last-child').attr('href')
+      , iconUrl: dict[userName]
+      }
+    }).get()
+  })
 })
 .then(commit)
