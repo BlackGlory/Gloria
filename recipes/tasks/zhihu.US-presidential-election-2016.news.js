2941aa560d19ee0895b04b5c553fed3d18cddc2d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2016-09-19 14:07:09 +0800

    update recipe

--- recipes/tasks/zhihu.US-presidential-election-2016.news.js
+++ recipes/tasks/zhihu.US-presidential-election-2016.news.js
@@ -4,7 +4,7 @@ Promise.all([
 ])
 .then(([{ cheerio }, html]) => {
   let $ = cheerio.load(html)
-  return $('.feed-item').map((i, el) => {
+  return $('.feed-item:not(.question-item)').map((i, el) => {
     return {
       iconUrl: 'https://pic1.zhimg.com/2e33f063f1bd9221df967219167b5de0_m.jpg'
     , message: ($(el).find('.author-link').text() || $(el).find('.zm-item-answer-author-info').text().trim()) + '回答了 ' + $(el).find('.question_link').text().trim()
